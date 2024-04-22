3a31196bea17c23f9765c2f82af630cbb71d5d12
Guozi <569431994@qq.com>
2024-04-22 21:03:42 +0800

    first commit

---
+++ src/main/java/com/wt/community/WTcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.wt.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WTcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WTcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wt/community/WTcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WTcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
